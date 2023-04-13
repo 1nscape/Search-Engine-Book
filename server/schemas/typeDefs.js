896fb5a947aecd7d83d820fa7aa6a3df9d5bf3c6
Max <max.belenchuk@gmail.com>
2023-04-13 19:38:04 -0300

    App.js modified according to instructions of README file. Updating react components to use with Apollo.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,7 +14,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: BookInput!): User
-    removeBook(bookId: ID): User
+    removeBook(DefsbookId: ID): User
   }
 
 
