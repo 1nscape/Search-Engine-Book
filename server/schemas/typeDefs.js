ca1cdc8c5f8c6123c96410c4930223a3de25a5e0
Max <max.belenchuk@gmail.com>
2023-04-13 16:55:34 -0300

    Defined typeDefs in typeDefs.js to use with resolvers in resolvers.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,56 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    bookCount: Int
+    email: String
+    savedBooks: [Book]
+  }
+
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+
+  type Auth {
+    user: User
+    token: ID!
+    
+  }
+
+
+  type Query {
+    me: User
+  }
+
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    image: String
+    title: String
+  }
+
+
+  input InputBook {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+
+  }
+
+
+
+`;
+
+module.exports = typeDefs;
