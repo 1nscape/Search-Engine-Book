9881e58e1b63766ea59d14778f95bb4ab12c483a
Max <max.belenchuk@gmail.com>
2023-04-13 18:33:51 -0300

    Defining resolvers. Login set up with authentication error including password and email. Adding user as well.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,47 @@
 
 
+const { signToken } = require("../utils/auth");
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const dataUser = await User.findOne({ _id: context.user._id }).select( "-__v -password");
+
+        return dataUser;
+      }
+
+      throw new AuthenticationError("Must be logged in!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError('Email does not exist in the system, please try a new email.')
+        }
+        const correctPassword = await user.isCorrectPassword(password);
+        if (!correctPassword) {
+            throw new AuthenticationError('Password is incorrect')
+        }
+        const token = signToken (user);
+    },
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return {token, user};
+
+    },
+    
+    
+
+  }
+
+}
+  
+
+module.exports = resolvers
